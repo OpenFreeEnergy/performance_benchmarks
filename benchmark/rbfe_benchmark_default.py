07c917ba388442dc5130397cbc4a72192bd5472f
IAlibay <IAlibay@users.noreply.github.com>
2025-09-29 23:55:35 +0100

    split things up

--- benchmark/rbfe_benchmark_default.py
+++ benchmark/rbfe_benchmark_default.py
@@ -22,7 +22,6 @@ def get_settings(waters):
     settings.simulation_settings.production_length = 500 * unit.picosecond
     settings.simulation_settings.real_time_analysis_interval = 100 * unit.picosecond
     settings.output_settings.checkpoint_interval = 100 * unit.picosecond
-    settings.output_settings.positions_write_frequency = 100 * unit.picosecond
     settings.protocol_repeats = 1
     settings.engine_settings.compute_platform = "cuda"
     settings.alchemical_settings.explicit_charge_correction = True
@@ -136,7 +135,12 @@ def run_inputs(pdb, cofactors, edge, waters):
                     stateB_dict[entry] = cofactor
 
         if edge is not None:
-            mapping = openfe.LigandAtomMapping.from_json(edge)
+            try:
+                mapping = openfe.LigandAtomMapping.from_json(edge)
+            except AttributeError:
+                with open(edge, 'r') as fd:
+                    mapping = openfe.LigandAtomMapping.from_dict(json.load(fd))
+
             stateA_dict["ligand"] = mapping.componentA
             stateB_dict["ligand"] = mapping.componentB
 
--- data/ross_cubic.py
+++ data/ross_cubic.py
@@ -1,35 +1,42 @@
 import pathlib
 import json
 
-glob_search = pathlib.Path("ross_2023/").rglob("*/*")
-targets = [p for p in glob_search if p.is_dir()]
+glob_search = pathlib.Path("ross_2023/").glob("*")
+systems = [p for p in glob_search if p.is_dir()]
 
-payload = {}
 
-for target in targets:
-    metadata = {}
-    name = target.name
-    metadata["edge"] = str(target / f"{name}_edge.json")
-    metadata["protein"] = str(target / "protein.pdb")
+for system in systems:
     
-    cofactors = target / 'cofactors.sdf'
-    if cofactors.exists():
-        metadata["cofactors"] = str(cofactors)
+    targets = [p for p in system.glob("*") if p.is_dir()]
+    payload = {}
     
-    with open(target / "waters_cubic_default.json", "r") as fd:
-        system_details = json.load(fd)
+    for target in targets:
+        metadata = {}
+        name = target.name
+        metadata["edge"] = str(target / f"{name}_edge.json")
+        metadata["protein"] = str(target / "protein.pdb")
+        
+        cofactors = target / 'cofactors.sdf'
+        if cofactors.exists():
+            metadata["cofactors"] = str(cofactors)
+        
+        with open(target / "waters_cubic_default.json", "r") as fd:
+            system_details = json.load(fd)
+        
+        metadata["atoms"] = {
+            "complex": system_details["complex"][1],
+            "solvent": system_details["solvent"][1],
+        }
     
-    metadata["atoms"] = {
-        "complex": system_details["complex"][1],
-        "solvent": system_details["solvent"][1],
-    }
-
-    metadata["waters"] = {
-        "complex": system_details["complex"][0],
-        "solvent": system_details["solvent"][0],
-    }
+        metadata["waters"] = {
+            "complex": system_details["complex"][0],
+            "solvent": system_details["solvent"][0],
+        }
+        
+        if name in payload:
+            payload[f"{name}_1"] = metadata
+        else:
+            payload[name] = metadata
     
-    payload[name] = metadata
-
-with open("ross_cubic.json", "w") as fd:
-    json.dump(payload, fd, indent=4)
+    with open(f"ross_cubic_{system.name}.json", "w") as fd:
+        json.dump(payload, fd, indent=4)
